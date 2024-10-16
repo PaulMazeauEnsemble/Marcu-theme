a2e69f4cadd78c6b43007cf079281094f64137ea
PaulMazeauEnsemble <paul@ensemble.ooo>
2024-10-16 11:57:36 +0200

    Header

--- header.php
+++ header.php
@@ -14,17 +14,17 @@
     <link href="https://fonts.googleapis.com/css2?family=DM+Mono:ital,wght@0,300;0,400;0,500;1,300;1,400;1,500&family=Instrument+Serif:ital@0;1&family=Space+Grotesk:wght@300..700&display=swap" rel="stylesheet">
 </head>
 <body <?php body_class(); ?>>
-    <div class="flex items-center justify-between">
+    <div class="fixed top-0 left-0 right-0 flex items-center justify-between bg-color-background z-50 px-4 py-3">
         <nav class="flex-1">
             <?php
             wp_nav_menu(array(
                 'theme_location' => 'left-menu',
                 'container' => false,
-                'menu_class' => 'flex space-x-4'
+                'menu_class' => 'flex space-x-4 text-base font-untitled font-light text-gray-400'
             ));
             ?>
         </nav>
-        <div class="h-20 w-20 flex justify-center">
+        <div id="logo" class="h-auto w-24 flex justify-center items-center transition-all duration-500 ">
             <?php the_custom_logo(); ?>
         </div>
         <nav class="flex-1 text-right">
@@ -32,10 +32,23 @@
             wp_nav_menu(array(
                 'theme_location' => 'right-menu',
                 'container' => false,
-                'menu_class' => 'flex space-x-4 justify-end'
+                'menu_class' => 'flex space-x-4 justify-end text-base font-untitled font-light text-gray-400'
             ));
             ?>
         </nav>
     </div>
+
+    <script>
+        window.addEventListener('scroll', function() {
+            const logo = document.getElementById('logo');
+            if (window.scrollY > window.innerHeight) {
+                logo.classList.add('h-auto', 'w-10');
+                logo.classList.remove('h-auto', 'w-24');
+            } else {
+                logo.classList.add('h-auto', 'w-24');
+                logo.classList.remove('h-auto', 'w-10');
+            }
+        });
+    </script>
 </body>
 </html>
