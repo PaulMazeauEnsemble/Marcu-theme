f35c5c6baa60d0feaf219ce34d6af5e9e2a197e7
PaulMazeauEnsemble <paul@ensemble.ooo>
2024-10-22 15:07:39 +0200

    Responsive page journal

--- journal.php
+++ journal.php
@@ -9,7 +9,7 @@ include('header.php');
 <div class="content bg-color-background pt-20 md:pt-44">
     <div class="pb-24 md:pb-36 border-b grid grid-cols-1 md:grid-cols-12">
         <h1 class="font-tiempos font-light text-4xl md:text-6xl text-center col-span-6 col-start-4 pb-9 md:pb-14"><?php the_title(); ?></h1>
-        <p class="font-untitled text-base md:text-base text-center col-span-6 col-start-4"><?php the_field('texte'); ?></p>
+        <p class="font-untitled text-base md:text-base text-center col-span-6 col-start-4 px-4 md:px-0"><?php the_field('texte'); ?></p>
     </div>
     <div class="articles-list">
         <?php
